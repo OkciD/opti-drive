42972969768bd8b78b0b8c83c9ae8964e64e70cd
Yury Baykov <yury.baykov@mail.ru>
2025-05-17 21:04:16 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import packageJson from '../package.json' with {type: 'json'};
+
+const sleep = (ms: number) =>
+	new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+
+await sleep(1000);
+
+console.log(`${packageJson.name}@${packageJson.version} start`);
+
+export {};
